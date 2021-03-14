9cfc8b9d837547e460677fca3e52cc1555cd5020
Gabriel Gallardo <pekena.larva@gmail.com>
2021-03-14 19:12:40 +0100

    Fixed ball X position

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,17 +6,26 @@ using extOSC;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] spherePrefabs;
-    public float spawnWhiteX;
-    public float spawnRedX;
-    public float spawnGreenX;
     public float spawnBlueX;
     public float spawnPinkX;
+    public float spawnWhiteX;
+    public float spawnGreenX;
+    public float spawnRedX;
+ 
     public float spawnY;
     //private float spawnDelay = 2;
 
     public float spawnRate;
 
     // Start is called before the first frame update
+    private void Awake()
+    {
+        SpawnSphereBlue();
+        SpawnSpherePink();
+        SpawnSphereWhite();
+        SpawnSphereGreen();
+        SpawnSphereRed();
+    }
 
     private void Start()
     {
@@ -26,48 +35,34 @@ public class SpawnManager : MonoBehaviour
         InvokeRepeating("SpawnSphereBlue", spawnRate, spawnRate);
         InvokeRepeating("SpawnSpherePink", spawnRate, spawnRate);
     }
-    void SpawnSpheres()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    { 
-    
-    }
-
-    public void SpawnSphereWhite()
-    {
-        Vector3 spawnPos = new Vector3(spawnWhiteX, spawnY, 0);
-        //spawns the object
-        Instantiate(spherePrefabs[0], spawnPos, spherePrefabs[0].transform.rotation);
-    }
-
-    public void SpawnSphereRed()
-    {
-
-        Vector3 spawnPos = new Vector3(spawnRedX, spawnY, 0);
-        //spawns the object
-        Instantiate(spherePrefabs[1], spawnPos, spherePrefabs[1].transform.rotation);
-    }
-
-    public void SpawnSphereGreen()
-    {
-        Vector3 spawnPos = new Vector3(spawnGreenX, spawnY, 0);
-        //spawns the object
-        Instantiate(spherePrefabs[2], spawnPos, spherePrefabs[2].transform.rotation);
-    }
-
     public void SpawnSphereBlue()
     {
         Vector3 spawnPos = new Vector3(spawnBlueX, spawnY, 0);
         //spawns the object
-        Instantiate(spherePrefabs[3], spawnPos, spherePrefabs[3].transform.rotation);
+        Instantiate(spherePrefabs[0], spawnPos, spherePrefabs[0].transform.rotation);
     }
     public void SpawnSpherePink()
     {
         Vector3 spawnPos = new Vector3(spawnPinkX, spawnY, 0);
+        //spawns the object
+        Instantiate(spherePrefabs[1], spawnPos, spherePrefabs[1].transform.rotation);
+    }
+    public void SpawnSphereWhite()
+    {
+        Vector3 spawnPos = new Vector3(spawnWhiteX, spawnY, 0);
+        //spawns the object
+        Instantiate(spherePrefabs[2], spawnPos, spherePrefabs[2].transform.rotation);
+    }
+    public void SpawnSphereGreen()
+    {
+        Vector3 spawnPos = new Vector3(spawnGreenX, spawnY, 0);
+        //spawns the object
+        Instantiate(spherePrefabs[3], spawnPos, spherePrefabs[3].transform.rotation);
+    }
+    public void SpawnSphereRed()
+    {
+
+        Vector3 spawnPos = new Vector3(spawnRedX, spawnY, 0);
         //spawns the object
         Instantiate(spherePrefabs[4], spawnPos, spherePrefabs[4].transform.rotation);
     }
